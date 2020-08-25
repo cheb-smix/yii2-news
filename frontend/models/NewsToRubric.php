93117aa41bd024913dd4fa8e95514013e1946c12
SMIX <dj-smix@mail.ru>
2020-08-25 15:48:02 +0300

    Added main models, controller and view for Rubric and News

---
+++ frontend/models/NewsToRubric.php
@@ -0,0 +1,37 @@
+<?php
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * NewsToRubric model
+ *
+ * @property integer $rubric_id
+ * @property integer $news_id
+ */
+
+class NewsToRubric extends ActiveRecord{
+	public function attributeLabels()
+	{
+		return [
+			'rubric_id'=>'ID рубрики',
+			'news_id'=>'ID новости'
+		];
+	}
+	public function rules()
+	{
+		return [
+			[ ['rubric_id','news_id'], 'required' ]
+		];
+	}
+	public function getRubric()
+	{
+		return $this->hasOne(Rubric::className(), ['id' => 'rubric_id']);
+    }
+    public function getNews()
+	{
+		return $this->hasOne(News::className(), ['id' => 'news_id']);
+	}
+}
+
---
+++ frontend/models/Rubric.php
@@ -0,0 +1,39 @@
+<?php
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * Rubric model
+ *
+ * @property integer $id
+ * @property string $title
+ * @property integer $parent_rubric_id
+ */
+
+class Rubric extends ActiveRecord{
+	public function attributeLabels()
+	{
+		return [
+			'title'=>'Заголовок',
+			'parent_rubric_id'=>'Родительская рубрика'
+		];
+	}
+	public function rules()
+	{
+		return [
+			[ ['title'], 'required' ],
+			[ ['title'], 'string', 'length'=>[5,70], 'message'=>'Wrong' ],
+		];
+	}
+	public function getChildren()
+	{
+		return $this->hasMany(Rubric::className(), ['parent_rubric_id' => 'id'])->with("children");
+    }
+    public function getNews()
+    {
+        return $this->hasMany(NewsToRubric::className(), ['rubric_id' => 'id'])->with("news");
+    }
+}
+
